6f03a33b435e3abee873d9833c9c460b9b05cb42
Delphine Grace Hoekelet <hoekeletd@gmail.com>
2018-11-26 17:43:42 +0100

    exo2

---
+++ javascript/lesson2/exercise2/main.js
@@ -0,0 +1,14 @@
+/* Il doit compter de 0 à 100. (afficher le nombre à chacune des itérations).*/
+for ( var i = 0 ; i < 100 ; i++ ) {
+    console.log("Je compte jusqu'à " + i);
+}
+
+/* Maintenant, transformez le pour qu'il compte de deux en deux.*/
+for(var i = 0 ; i< 100 ; i+2);{
+    console.log( "je compte de deux en deux" + i)
+
+}
+
+/* Inversez le pour qu'il décompte de 2 en 2 de 100 à 0.*/
+for(var i = 100 ; i> 100 ; i+2);{
+    console.log( "je décompte de deux en deux" + i)
