1e143a764eb378e3c81831651716953a05938fba
Delphine Grace Hoekelet <hoekeletd@gmail.com>
2018-11-26 17:55:10 +0100

    exo

--- javascript/lesson2/exercise2/main.js
+++ javascript/lesson2/exercise2/main.js
@@ -2,13 +2,11 @@
 for ( var i = 0 ; i < 100 ; i++ ) {
     console.log("Je compte jusqu'à " + i);
 }
-
 /* Maintenant, transformez le pour qu'il compte de deux en deux.*/
-for(var i = 0 ; i< 100 ; i+2);{
-    console.log( "je compte de deux en deux" + i)
-
+for(var i = 0 ; i< 100 ; i= i + 2);{
+    console.log( "je compte de deux en deux")
 }
-
 /* Inversez le pour qu'il décompte de 2 en 2 de 100 à 0.*/
-for(var i = 100 ; i> 100 ; i+2);{
-    console.log( "je décompte de deux en deux" + i)
+for(var i = 100 ; i> 0 ; i= i - 2);{
+    console.log( "je décompte de deux en deux")
+}
