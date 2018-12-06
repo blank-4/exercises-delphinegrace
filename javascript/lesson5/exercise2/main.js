5a9ead31dc3f63b1bc0bee37022c675b19e77bb8
Delphine Grace Hoekelet <hoekeletd@gmail.com>
2018-12-06 16:37:24 +0100

    exos 2

---
+++ javascript/lesson5/exercise2/main.js
@@ -0,0 +1,15 @@
+//Dans votre script créez une fonction qui, elle contiendra le code pour les consignes suivantes :
+/*Créer une div
+* Attribuez lui une taille et une background-color avec JS.
+* Lorsque l'on passe la souris sur la div elle devra changer de couleur de fond .*/
+
+FUNCTION WhenCalled( ) {
+    var div = document.createElement('div');
+    document.body.appendChild(div);
+    div.style.width = '200px';
+    div.style.height = '40px';
+    div.onmouseover = function( 
+        div.style.backgroundcolor = 'blue';
+    )
+}
+whenCalled();
