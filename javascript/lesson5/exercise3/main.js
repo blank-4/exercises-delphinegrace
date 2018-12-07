7accc58aeb29b66d438082b983c23c0297aa09b2
Delphine Grace Hoekelet <hoekeletd@gmail.com>
2018-12-07 16:11:48 +0100

    exo correction

--- javascript/lesson5/exercise3/main.js
+++ javascript/lesson5/exercise3/main.js
@@ -1,4 +1,5 @@
-var str = document.getElementById("monTableau"); str.innerHTML = "lorem ipsum";
+var str = document.getElementById("monTableau");
+str.innerHTML = "lorem ipsum";
 
 function img() {
     var imgs = ['"https://i.pinimg.com/474x/fd/e6/b1/fde6b17f014fea2c372fa18ab03ed9b4--dog-smiling-card-ideas.jpg"','"http://www.vitamin-ha.com/wp-content/uploads/2014/04/VH-Random-duckhourse.jpg','"http://kb4images.com/images/random-image/37670495-random-image.jpg"', 'http://saxony-blue.com/data/out/154/6565303-random-picture.jpg"'];
---
+++ javascript/lesson5/exercise4/style.js
@@ -0,0 +1,16 @@
+// on crée quatre variables avec une même instruction
+function TABLEAU() {
+var monObj = new Object(),
+    str = "myString",
+    rand = Math.random(),
+    obj = new Object();
+
+monObj.type              = "bjakfb  oùf";
+monObj[" ACHKA CKA C"]   = " bofbaeofbiaPOFBPI";
+monObj[str]              = "njz ofb z   pofpi   f";
+monObj[rand]             = "fbeaofbvoeaBFVJO";
+monObj[obj]              = "Oefb hIV EOt";
+monObj[""]               = "z fhf ejoa VJA Vjp  ";
+
+console.log([monObj]);
+}
