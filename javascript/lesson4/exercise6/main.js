fd118743990b26631bcab408e33094b2e03dba7d
delphinegrace <44778581+delphinegrace@users.noreply.github.com>
2018-12-10 10:58:50 +0100

    Java lesson 5 (#14)
    * exo
    
    * exo
    
    * exos 1 lesson 5
    
    * exos 2
    
    * exos
    
    * exo correction

--- javascript/lesson4/exercise6/main.js
+++ javascript/lesson4/exercise6/main.js
@@ -1,34 +1,24 @@
-/*### CHIFFREMENT DE CÉSAR
-
-* Avant tout, aller regarder ce qu'est le chiffre de César, ou chiffrement par décallage : [https://fr.wikipedia.org/wiki/Chiffrement_par_d%C3%A9calage](https://fr.wikipedia.org/wiki/Chiffrement_par_d%C3%A9calage)
-
-* Créer une fonction `chiffrer`. Elle prendra deux paramètres,  le premier est une String qui sera la phrase à encoder. Le second est le décalage pour le code de César :
-  * Cette fonction doit encoder le message et retourner le message encodé.
-  * Pour vous aider: voici l'alphabet sous forme de tableau :
-  ```js
-  var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  ```
-* Créer une fonction `dechiffrer` qui prendra les deux mêmes paramètres que la fonction chiffrer.
-  * Cette fonction doit déchiffrer le message passé en paramètre avec la clé, elle aussi passée en paramètre.
-  * 
-  
-* Bonus : le programme doit laisser tel quel les charactères non présents dans l'alphabet.
-*/
-
-function chiffrer(str, key){
-    var res = "";
+function chiffrer(str, key) {
     var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var res = "";
     for (var i = 0; i < str.length; i++) {
         // récupérer l'index de la lettre
-        var letters = str[i];
-        console.log("pour la lettre" +letters)
-        var index = alphabet.indexOf(letters);
-        console.log("l'index de départ est:" + index);
-        // décaler l'index de "key" pour avoir une lettre
-        var nouvelleLettre = alphabet[index];
-        console.log("la nouvelle lettre est:" + nouvelleLettre);
+        var l = str[i];
+        console.log("pour la lettre " + l);
+        var index = alphabet.indexOf(l);
+        console.log("l'index initial est : " + index);
+        // décaler l'index de "key" pour avoir une nouvelle lettre
+        index = index + key;
+        console.log("l'index décalé est : " + index);
+        // trouver la lettre qui est située au nouvel index
+        var nouvelleLettre = alphabet[index]; 
+        console.log("la nouvelle lettre est : " + nouvelleLettre);
+        // ajout de la nouvelle lettre à la fin du résultat
         res = res + nouvelleLettre;
     }
     return res;
+    
+        
 }
-chiffrer("coucou", 2);
+
+console.log(chiffrer("bienenue",5));
