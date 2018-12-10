3484afe1a00090d7b5d6eb40a0bdd461e346ee14
Delphine Grace Hoekelet <hoekeletd@gmail.com>
2018-12-10 17:06:15 +0100

    exo 1  lesson6

---
+++ javascript/lesson6/exercise2/main.js
@@ -0,0 +1,15 @@
+var request = new XMLHttpRequest(); //création de la requête
+request.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) { //ici on vérifie que si l'état change
+        // il faut qu'il soit bon. Status : 200 signifie que la requête s'est bien passée
+        // readyState : 4 signifie que la requête est finie et que la réponse est prête
+        var jsonData = JSON.parse(this.responseText)
+        console.log (jsonData.records);
+        for (var i = 0; i < jsonData.records.length; i++){
+            console.log(jsonData.records[i].fields.designation_longue);
+        }
+    }
+};
+request.open("GET", "http://localhost:8000/data.json", true); //spécification de la
+// requête, l'adresse et si la requête est asynchrone (non bloquante)
+request.send();
