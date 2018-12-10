56e8608bd2eebefec38f905d77b0d25c6fea9502
delphinegrace <44778581+delphinegrace@users.noreply.github.com>
2018-12-10 10:57:46 +0100

    Js lesson3 (#12)
    * exo
    
    * exos 1 lesson3 yeaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
    
    * yy

---
+++ javascript/lesson3/exercise1/main.js
@@ -0,0 +1,12 @@
+//* je déclare une fonction, choisir un nom pour celle-ci.
+//function helloworld(){
+    
+//}
+//* On va ensuite lui donner des instructions.
+//On veut que la fonction écrive "hello world !" dans la console.
+function showhelloworld() {
+    console.log("helloworld");
+}
+showhelloworld();
+
+ 
